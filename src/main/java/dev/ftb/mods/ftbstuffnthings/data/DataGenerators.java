2d64c65c8f786f84c339bef56cbf39fe0e892435
Michael Hillcox <michaelhillcox@gmail.com>
2026-05-13 16:57:18 +0100

    fix: minor thing

--- src/main/java/dev/ftb/mods/ftbstuffnthings/data/DataGenerators.java
+++ src/main/java/dev/ftb/mods/ftbstuffnthings/data/DataGenerators.java
@@ -8,9 +8,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.Set;
